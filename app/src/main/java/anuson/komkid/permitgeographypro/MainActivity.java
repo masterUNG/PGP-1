980b856bf3d92d78932395696eb6a26fc2ed2d30
game <game4670@gmail.com>
2016-10-02 09:26:23 +0700

    master

--- app/src/main/java/anuson/komkid/permitgeographypro/MainActivity.java
+++ app/src/main/java/anuson/komkid/permitgeographypro/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
