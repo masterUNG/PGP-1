b0cd14828f7770d1919359f44a5aae04a28fee1a
game <game4670@gmail.com>
2016-10-02 10:45:46 +0700

    Chak User ได้เเล้ว

--- app/src/main/java/anuson/komkid/permitgeographypro/Register_user.java
+++ app/src/main/java/anuson/komkid/permitgeographypro/Register_user.java
@@ -2,7 +2,9 @@ package anuson.komkid.permitgeographypro;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +17,9 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -58,9 +63,70 @@ public class Register_user extends Activity {
             myAlert.myDialog(this, "รหัสไม่ตรงกัน","กรุณากรอกรหัสผ่านให้ตรงกันครับ");//เช็ครหัสผ่านให้ตรง
 
         }else {
-            confirmData();
+
+            CheckUser checkUser = new CheckUser(this);
+            checkUser.execute();
         }
     }//clickSaveData
+
+    private class CheckUser extends AsyncTask<Void, Void, String>{
+        //Explict
+        private Context context;
+        private static final String urlJSON="http://swiftcodingthai.com/gam/php_get_member_user.php";
+        private boolean aBoolean = true;
+
+        public CheckUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new  Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            }catch (Exception e){
+                Log.d("2octV1","e doInBack ==>" + e.toString());
+                return null;
+            }
+        }//DoInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("2octv1","JSON==>" + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i+=1){
+                    JSONObject jsonObject= jsonArray.getJSONObject(i);
+                    if (usernameString.equals(jsonObject.getString("mem_u_user"))) {
+
+                        aBoolean = false; //User ซ้ำ
+
+                    }//if
+                }//for
+                if (aBoolean) {
+                    //User OK
+                    confirmData();
+                }else {
+                    //User Not OK
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context,"User ซ้ำ",
+                            "กรุณาเปลี่ยน ชื่อผู้ใช้งาน");
+
+                }
+
+            }catch (Exception e ){
+                e.printStackTrace();
+            }
+        }//onPost
+    }//CheckUser Class
 
    private void confirmData() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
